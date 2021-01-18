496218e236934f4a20521d6be844c8ce7df5b0bc
theusaf <theusafyt@gmail.com>
2021-01-18 02:32:52 -0800

    almost finish ui

--- tos-xml.userscript.js
+++ tos-xml.userscript.js
@@ -13,13 +13,13 @@
 // @license      MIT
 // ==/UserScript==
 
-// e.target.result.xhr (.resposne = arraybuffer)
-// https://stackoverflow.com/questions/6965107/converting-between-strings-and-arraybuffers
-//
-// e.target.result.url ==== "https://www.blankmediagames.com/TownOfSalem/Unity/WebAssets.3.3.4/XMLData/StringTable.en-US.xml"
+/* global TOSXML_Data */
 
 if (window.TOSXML_Loaded || window.parent.TOSXML_Loaded) {
   throw "[TOSXML] - Already loaded";
+}
+if (!localStorage.TOSXML_Replacements){
+  localStorage.TOSXML_Replacements = "{}";
 }
 document.write("[TOSXML] - Patching Town of Salem. Please wait. If this screen stays blank for long periods of time, this userscript may not be working properly. Disable it and send a report to theusaf.");
 const mainPage = new XMLHttpRequest();
@@ -42,7 +42,7 @@ mainPage.onload = function(){
             XMLParser = new DOMParser,
             XMLText = decoder.decode(data.target.result.xhr.response),
             XMLData = XMLParser.parseFromString(XMLText,"text/xml").documentElement,
-            {TOSXML_Replacements} = localStorage,
+            TOSXML_Replacements = JSON.parse(localStorage.TOSXML_Replacements),
             warn = [];
           window.parent.TOSXML_Data = {
             edited: XMLData,
@@ -59,12 +59,6 @@ mainPage.onload = function(){
                   reason: "Key Missing"
                 });
                 continue;
-              }
-              const itemText = item.textContent;
-              if(TOSXML_Replacements[i].original !== itemText){
-                warn.push({
-                  key: TOSXML_Replacements[i].key
-                });
               }
               item.innerHTML = TOSXML_Replacements[i].value;
             }
@@ -107,15 +101,38 @@ mainPage.onload = function(){
         color: white;
         padding: 0.5rem;
         border-radius: 0.5rem;
-        max-height: 100%;
-        overflow: auto;
       }
       #TOSXML_Main:hover{
         background: black;
       }
+      details>div{
+        display: flex;
+      }
+      details>div>div{
+        flex: 1;
+        overflow: auto;
+        border-radius: 0.5rem;
+        border: solid #666 0.25rem;
+        height: calc(100vh - 6rem);
+      }
+      details>div>div>div>div{
+        padding: 0.25rem;
+      }
+      details>div>div>div>div:nth-child(2n){
+        background: #444;
+      }
+      details>div>div>div>div:nth-child(2n+1){
+        background: #222;
+      }
+      code{
+        background: black;
+        border-radius: 0.5rem;
+        padding: 0.25rem;
+      }
     </style>
     <details>
       <summary>TOSXML 1.0.0 @theusaf</summary>
+      <p>Here, you can edit keys. However, changes will only take effect on reload.</p>
       <div id="TOSXML_Container">
         <div id="TOSXML_AllKeys">
           <span>All Keys</span>
@@ -124,13 +141,90 @@ mainPage.onload = function(){
         <div id="TOSXML_SavedEdits">
           <span>Your Edits</span>
           <div></div>
+          <button>New Edit</button>
         </div>
         <div id="TOSXML_EditWarnings">
-          <span>Warnings</span>
+          <span>Errors/Warnings</span>
           <div></div>
         </div>
       </div>
     </details>`;
     document.body.append(settingsDiv);
+    function sanitize(data){
+      return data.replace(/</mg,"&lt;").replace(/>/mg,"&gt;");
+    }
+    const awaiter = setInterval(()=>{
+      if(typeof TOSXML_Data !== "undefined"){
+        clearInterval(awaiter);
+        const {original,warn} = TOSXML_Data,
+          edited = JSON.parse(localStorage.TOSXML_Replacements),
+          itemsAll = document.querySelector("#TOSXML_AllKeys>div"),
+          itemsEdit = document.querySelector("#TOSXML_SavedEdits>div"),
+          warnings = document.querySelector("#TOSXML_EditWarnings>div"),
+          addButton = document.querySelector("#TOSXML_SavedEdits>button");
+        addButton.onclick = ()=>{
+          itemsEdit.append(newEdit());
+        };
+        for(let i = 0; i < original.children.length; i++){
+          const item = original.children[i],
+            e = document.createElement("div");
+          e.innerHTML = `<code class="TOSXML_key">${item.getAttribute("key")}</code>
+ - <code class="TOSXML_value">${sanitize(item.textContent)}</code>`;
+          itemsAll.append(e);
+        }
+        for(const i in edited.length){
+          itemsEdit.append(newEdit(edited[i]));
+        }
+        for(let i = 0; i < warn.length; i++){
+          const item = warn[i],
+            e = document.createElement("div");
+          e.innerHTML = `<code class="TOSXML_key">${item.key}</code>
+ - <code class="TOSXML_value">${item.reason}</code>`;
+          warnings.append(e);
+        }
+      }
+    },500);
+    function newEdit(info){
+      const e = document.createElement("div"),
+        v = document.createElement("input"),
+        d = document.createElement("button"),
+        s = document.createElement("button");
+      let k;
+      s.innerHTML = "✔️";
+      d.innerHTML = "X";
+      v.placeholder = "Value";
+      function save(){
+        const data = JSON.parse(localStorage.TOSXML_Replacements);
+        if(k.nodeName === "INPUT"){
+          const n = document.createElement("code");
+          n.innerHTML = k.value;
+          k.replaceWith(n);
+          k = n;
+        }
+        data[k.value || k.textContent] = {
+          value: v.value,
+          key: k.value || k.textContent
+        };
+        localStorage.TOSXML_Replacements = JSON.stringify(data);
+      }
+      function del(){
+        e.outerHTML = "";
+        const data = JSON.parse(localStorage.TOSXML_Replacements);
+        delete data[k.value || k.textContent];
+        localStorage.TOSXML_Replacements = JSON.stringify(data);
+      }
+      if(info){
+        k = document.createElement("code");
+        k.textContent = info.key;
+        v.value = sanitize(info.value);
+      }else{
+        k = document.createElement("input");
+        k.placeholder = "Key";
+      }
+      d.onclick = del;
+      s.onclick = save;
+      e.append(k,v,s,d);
+      return e;
+    }
   };
 };
